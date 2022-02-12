28ea5af862c125ef3c7de2959508bb8acbfd00a9
ChandraPrakash <81704493+BSPCPrakash@users.noreply.github.com>
2022-02-12 13:34:26 +0530

    Frontend-commit
    Navbar commit

--- ml-bot/src/App.js
+++ ml-bot/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/NavBar';
+import Home from "./components/Home.component.js";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+  
+    <Navbar/>
+  
   );
 }
 
